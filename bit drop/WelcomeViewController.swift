4124b2a76226ffcad3b1872d4a9acea149626abb
Andrew Breckenridge <asbreckenridge@me.com>
2015-08-22 19:31:15 -0700

    init

---
+++ bit drop/WelcomeViewController.swift
@@ -0,0 +1,37 @@
+//
+//  WelcomeViewController.swift
+//  bit drop
+//
+//  Created by Andrew Breckenridge on 8/22/15.
+//  Copyright © 2015 Andrew Breckenridge. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet weak var coinImageView: UIImageView!
+    @IBOutlet weak var slotImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        <#code#>
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
